de131adf0481ec0073384e81526a3ba0088a1deb
ramurayudu <52094022+ramurayudu@users.noreply.github.com>
2021-02-04 16:44:35 +0530

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,7 +2,7 @@ import spark.servlet.SparkApplication;
 
 import static spark.Spark.get;
 
-public class HelloWorld implements SparkApplication {
+public class HelloWold implements SparkApplication {
 	public static void main(String[] args) {
 		new HelloWorld().init();
 	}
